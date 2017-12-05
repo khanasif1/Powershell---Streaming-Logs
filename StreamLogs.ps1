5974078965a0e8f419b64cd5be09d53fa87fd672
Asif Khan <Asif Khan@au.aap.ad.pwcinternal.com>
2017-12-06 10:43:56 +1100

    Basic script to stream log from log file. Also manuplate text and forground colour

---
+++ StreamLogs.ps1
@@ -0,0 +1,15 @@
+﻿#Get-Content "C:\Dev\PWC.VSO\AU DigiTech FSA\D\V1.0\PWC.AU.FSA.AuditSherpa\PWC.AU.FSA.Automation\bin\Logs\Info\1.log" -WAIT
+
+$file="C:\Dev\PWC.VSO\AU DigiTech FSA\D\V1.0\PWC.AU.FSA.AuditSherpa\PWC.AU.FSA.Automation\bin\Logs\Info\1.log"
+Get-Content $file -Wait | ForEach-Object -Begin {
+  $counter = 1
+  $lines = @(Get-Content $file).Count
+} -Process {
+  if ($counter++ -gt $lines) {
+   if($_.ToString().Contains("ERROR")){
+       Write-host $_.ToString() -foregroundcolor "red"
+   }else{
+        Write-host $_.ToString() -foregroundcolor "green"
+   }    
+  }
+}
